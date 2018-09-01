bd55de667195c34efa5be6b5bc130bba38e64c13
Dareniar <DanilShchegol@icloud.com>
2018-09-01 14:09:57 +0300

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Данил on 9/1/18.
+//  Copyright © 2018 Dareniar. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var checked: Bool = false
+}
